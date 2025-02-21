d8769f36dee6145482110440963f7220b53a71c4
Bernard Lawes <bernardlawes@gmail.com>
2025-02-21 15:43:47 -0500

    added ssh

---
+++ SSH_Console/SSH_Console/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using SSH_Console;
+using SshKeyGenerator;
+
+
+
+class smartSSH
+{
+
+    public static bool overwrite = false;
+    static void Main(string[] args)
+    {
+
+
+        string type = "basic";
+        int strength = 2048;
+        string comment = string.Empty;
+        string foldername = "Not Set";
+
+
+        int[] validstrengths = { 1024, 2048, 4096 };
+        string[] validTypes = { "basic", "base64", "all" };
+
+        int i = 0;
+
+        foreach (string arg in args)
+        {
+
+
+
+            switch (arg)
+            {
+                case "-strength":
+                    try
+                    {
+                        strength = int.Parse(args[i + 1]);
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\nERROR : Strength value expected (-s).");
+                        return;
+                    }
+                    break;
+                case "-folder":
+                    try
+                    {
+                        foldername = args[i + 1];
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\nERROR: Folder name expected (-d)...: \nFolder: " + foldername + "\n");
+                        return;
+                    }
+                    break;
+                case "-type":
+                    try
+                    {
+                        type = args[i + 1];
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\nERROR: SSH Key Type expected (-o)...: \nDefaulting to 'basic'\n");
+                        type = "Basic";
+                    }
+                    type = char.ToUpper(type[0]) + type.Substring(1);
+                    break;
+                case "-comment":
+                    try
+                    {
+                        comment = args[i + 1];
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\nERROR: Comments expected (-d)...: \nComment: " + comment + "\n");
+                        return;
+                    }
+                    break;
+
+                case "/overwrite":
+                    overwrite = true;
+                    break;
+
+                default:
+                    break;
+            }
+            i++;
+        }
+
+
+        // Validate Strength
+        if (!validstrengths.Contains(strength)) strength = 2048;
+
+        // Validate Options
+        if (!validTypes.Contains(type.ToLower())) type = "Basic";
+
+        // Validate Folder
+        if (!(foldername == "Not Set") && !System.IO.Directory.Exists(foldername))
+        {
+            if (System.IO.Path.IsPathFullyQualified(foldername)) System.IO.Directory.CreateDirectory(foldername);
+            if (!System.IO.Directory.Exists(foldername))
+            {
+                Console.WriteLine("\nInvalid Folder: " + foldername + "\n");
+                return;
+            }
+        }
+
+
+
+        Console.WriteLine("\nStrength = " + strength.ToString());
+        Console.WriteLine("Folder = " + foldername);
+        Console.WriteLine("Type = " + type);
+        Console.WriteLine("Comment = " + comment);
+        Console.WriteLine("Overwrite = " + overwrite);
+
+
+        switch (type)
+        {
+            /*
+            case "Basic":
+                Print_SSH_KeyPair(strength);
+                break;
+            */
+            case "All":
+                ssh_generator.Print_SSH_KeyPair_All(strength, comment, foldername, overwrite);
+                break;
+            case "Base64":
+                ssh_generator.Print_SSH_KeyPair_Base64(strength, comment, foldername, overwrite);
+                break;
+            default:
+                ssh_generator.Print_SSH_KeyPair_Basic(strength, comment, foldername, overwrite);
+                break;
+        }
+
+    }
+
+}
---
+++ SSH_Console/SSH_Console/ssh_generator.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSH_Console
+{
+    class ssh_generator
+    {
+
+        public static void Print_SSH_KeyPair_Basic(int strength = 2048, string comment = "user@host", String Foldername = "", bool overwrite = false)
+        {
+
+            var keygen = new SshKeyGenerator.SshKeyGenerator(strength);
+
+            var privateKey = keygen.ToPrivateKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPrivate Key:\n\n" + privateKey + "\n");
+
+            var publicKey = keygen.ToPublicKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic Non RFC Key:\n\n" + publicKey + "\n");
+
+            var publicSshKey = keygen.ToRfcPublicKey(comment);
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic RFC Key:\n\n" + publicSshKey + "\n");
+
+
+
+
+            if (System.IO.Directory.Exists(Foldername))
+            {
+                Console.WriteLine("Folder exist: " + Foldername);
+                string[] files = { System.IO.Path.Combine(Foldername, "ssh_private.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public_rfc.key\"") };
+
+                foreach (string file in files)
+                {
+                    if (!overwrite && System.IO.File.Exists(file))
+                    {
+                        Console.WriteLine("A file with the name: " + file + "already exists in this path.  To overwrite files, set the Overwrite (/overwrite)");
+                        return;
+                    }
+                }
+
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_private.key"), privateKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public.key"), publicKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public_rfc.key"), publicSshKey);
+            } else
+            {
+                Console.WriteLine("Folder does not exist: "+Foldername); 
+            }
+
+        }
+        public static void Print_SSH_KeyPair_Base64(int strength = 2048, string comment = "user@host", String Foldername = "", bool overwrite = false)
+        {
+            // Create a new instance 
+            var keygen = new SshKeyGenerator.SshKeyGenerator(strength);
+
+            var privateKey = keygen.ToPrivateKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPrivate Key:\n\n" + privateKey + "\n");
+
+            var publicKey = keygen.ToPublicKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic Non RFC Key:\n\n" + publicKey + "\n");
+
+            var publicSshKey = keygen.ToRfcPublicKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic RFC Key:\n\n" + publicSshKey + "\n");
+
+            var publicSshKeyWithComment = keygen.ToRfcPublicKey(comment);
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic Key with Comment:\n\n" + publicSshKeyWithComment + "\n");
+
+            // Get Base64 encoded keys with private key
+            var sshBase64Keys = keygen.ToB64Blob(true);
+            Console.WriteLine("---------------------------------------------------------------------------------------\nBase64 Encoded Keys:\n\n" + sshBase64Keys + "\n");
+
+            var blobKeys = keygen.ToXml();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nXML Keys:\n\n" + blobKeys + "\n");
+
+            if (System.IO.Directory.Exists(Foldername))
+            {
+
+                string[] files = { System.IO.Path.Combine(Foldername, "ssh_private.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public_rfc.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public_comment.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_base64.key"),
+                             };
+
+                foreach (string file in files)
+                {
+                    if (!overwrite && System.IO.File.Exists(file))
+                    {
+                        Console.WriteLine("\nFile Write Cancelled\nA file with the name: " + file + "already exists in this folder.\nTo overwrite existing files, set the Overwrite (/overwrite).  ");
+                        return;
+                    }
+                }
+
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_private.key"), privateKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public.key"), publicKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public_rfc.key"), publicSshKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public_comment.key"), publicSshKeyWithComment);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_base64.key"), sshBase64Keys);
+            }
+
+
+        }
+
+        public static void Print_SSH_KeyPair_All(int strength = 2048, string comment = "user@host", String Foldername = "", bool overwrite=false)
+        {
+            // Create a new instance 
+            var keygen = new SshKeyGenerator.SshKeyGenerator(strength);
+
+            var privateKey = keygen.ToPrivateKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPrivate Key:\n\n" + privateKey + "\n");
+
+            var publicKey = keygen.ToPublicKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic Non RFC Key:\n\n" + publicKey + "\n");
+
+            var publicSshKey = keygen.ToRfcPublicKey();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic RFC Key:\n\n" + publicSshKey + "\n");
+
+            var publicSshKeyWithComment = keygen.ToRfcPublicKey(comment);
+            Console.WriteLine("---------------------------------------------------------------------------------------\nPublic Key with Comment:\n\n" + publicSshKeyWithComment + "\n");
+
+            // Get Base64 encoded keys with private key
+            var sshBase64Keys = keygen.ToB64Blob(true);
+            Console.WriteLine("---------------------------------------------------------------------------------------\nBase64 Encoded Keys:\n\n" + sshBase64Keys + "\n");
+
+            var sshXmlKeys = keygen.ToXml();
+            Console.WriteLine("---------------------------------------------------------------------------------------\nXML Keys:\n\n" + sshXmlKeys + "\n");
+
+
+            if (System.IO.Directory.Exists(Foldername))
+            {
+
+                string[] files = { System.IO.Path.Combine(Foldername, "ssh_private.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public_rfc.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_public_comment.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_base64.key"),
+                               System.IO.Path.Combine(Foldername, "ssh_xml.key")
+                             };
+
+                foreach (string file in files)
+                {
+                    if (!overwrite && System.IO.File.Exists(file))
+                    {
+                        Console.WriteLine("\nFile Write Cancelled\nA file with the name: " + file + "already exists in this folder.\nTo overwrite existing files, set the Overwrite (/overwrite).  ");
+                        return;
+                    }
+                }
+
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_private.key"), privateKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public.key"), publicKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public_rfc.key"), publicSshKey);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_public_comment.key"), publicSshKeyWithComment);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_base64.key"), sshBase64Keys);
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Foldername, "ssh_xml.key"), sshXmlKeys);
+            }
+
+
+        }
+    }
+}
