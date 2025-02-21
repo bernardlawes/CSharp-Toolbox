7f032d934c2db741a5aca157b91d2461708b26d1
Bernard Lawes <bernardlawes@gmail.com>
2025-02-21 15:05:02 -0500

    added ffmpeg tools

---
+++ Audio_Video_FFMPEG/Audio_Video_FFMPEG/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Audio_Video_FFMPEG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter path to video file:");
+            string videoFile = Console.ReadLine();
+            Console.WriteLine("Please enter output path");
+            string outputFolder = Console.ReadLine();
+            Console.WriteLine("Please enter path to FFMPEG Library");
+            string ffmpegFolder = Console.ReadLine();
+            Console.WriteLine("Please enter start time in minutes and seconds 00:00");
+            string startTime = Console.ReadLine();
+            Console.WriteLine("Please enter end time in minutes and seconds 00:00");
+            string endTime = Console.ReadLine();
+            
+            // Loop until a valid integer is entered for duration
+            int duration;
+            while (true)
+            {
+                Console.WriteLine("Please enter duration in seconds");
+                string durationStr = Console.ReadLine();
+                if (int.TryParse(durationStr, out duration))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid duration. Please enter a valid integer.");
+                }
+            }
+
+            // Loop until a valid integer is entered for quality
+            int quality;
+            while (true)
+            {
+                Console.WriteLine("Please enter quality (default is 0)");
+                string qualityStr = Console.ReadLine();
+                if (int.TryParse(qualityStr, out quality))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid quality. Please enter a valid integer.");
+                }
+            }
+
+            Console.WriteLine("You entered: \n" + 
+                            "Video File: "+ videoFile + "\n"+ 
+                            "Output Folder: " + outputFolder + "\n" +
+                            "FFMPEG Folder: " + ffmpegFolder + "\n" +
+                            "Start Time: " + startTime + "\n" +
+                            "End Time: " + endTime + "\n" +
+                            "Duration: " + duration + "\n" +
+                            "Quality: " + quality + "\n");
+
+
+            String msg = ffmpeg_utils.ExtractAudio(videoFile, outputFolder, ffmpegFolder, startTime, endTime, duration, quality);
+
+            String title = msg.ToLower().Contains("error") || msg.ToLower().Contains("fail") ? "Error" : "Information";
+
+        }
+    }
+}
---
+++ Audio_Video_FFMPEG/Audio_Video_FFMPEG/ffmpeg_utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Audio_Video_FFMPEG
+{
+    class ffmpeg_utils
+    {
+        public static String ExtractAudio(String FileIn, String FolderOut, String Folder_ffmpeg, String StartTime, String EndTime, Int32 Duration, Int32 Quality)
+        {
+
+            String msg = String.Empty;
+
+
+            // ----------------- Validate Mandatory Parameters --------------------------------
+
+
+
+            if (!System.IO.File.Exists(FileIn))
+            {
+                msg = "Error: Input File Not Found, " + FileIn;
+            }
+
+            if (System.IO.Path.GetExtension(FileIn).ToLower() != ".mp4")
+            {
+                msg = "Error: Input File must be of type (.mp4)";
+            }
+
+            if (!System.IO.Path.IsPathRooted(FolderOut))
+            {
+                msg = "Output Folder must be Fully Rooted: " + FolderOut;
+            }
+
+            if (!System.IO.File.Exists(Folder_ffmpeg + "bin\\" + "ffmpeg.exe"))
+            {
+                msg = "Error: Please provide a valid path to your FFMPEG directory: " + Folder_ffmpeg;
+            }
+
+            if (msg.Length > 0)
+            {
+                return msg;
+            }
+
+            string FileOut = System.IO.Path.Combine(FolderOut, System.IO.Path.GetFileNameWithoutExtension(FileIn) + ".mp3");
+
+            // ----------------- Validate Optional Parameters --------------------------------
+
+            StartTime = (StartTime.Length > 0) ? " -ss " + StartTime : "";
+            EndTime = (EndTime.Length > 0) ? " -to " + EndTime : "";
+            String DurationStr = (EndTime.Length == 0 && Duration > 0) ? " -t " + TimeSpan.FromSeconds(Duration).ToString(@"hh\:mm\:ss") : "";
+            String QualityStr = (Quality < 0) ? "0" : Quality.ToString();
+
+            // ----------------- Execute Process via SHELL --------------------------------
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = Folder_ffmpeg + "bin\\" + "ffmpeg";
+            process.StartInfo.Arguments = " -i \"" + FileIn + "\" " + StartTime + " " + EndTime + " " + DurationStr + " -q:a " + QualityStr + " -map a -y \"" + FileOut + "\"";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();
+
+            bool result = System.IO.File.Exists(FileOut);
+
+            msg = (result) ? "Audio Extraction Successful" : "Audio Extraction Failed";
+
+            return msg;
+
+
+
+
+        }
+    }
+}
